0503ac18083ebe6db92dc5ebc52a3d0312036254
Agusto Portela <agustoportelaa@gmail.com>
2020-10-15 00:16:17 -0300

    agustoportela

---
+++ TuLuz/TuLuz/Forums/Reportes/Empleados_Jefes/Empleados_Jefes.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace TuLuz.Forums.Reportes.Empleados_Jefes
+{
+    partial class Empleados_Jefes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.label1 = new System.Windows.Forms.Label();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ReporteEmpleados_Jefe = new TuLuz.Forums.Reportes.Empleados_Jefes.ReporteEmpleados_Jefe();
+            this.DataTable1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ReporteEmpleados_Jefe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(867, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Listado de Empleados (Jefes o Supervisores)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // reportViewer1
+            // 
+            reportDataSource4.Name = "DataSet1";
+            reportDataSource4.Value = this.DataTable1BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "TuLuz.Forums.Reportes.Empleados_Jefes.ReporteEmpleados_Jefes.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(1, 103);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(927, 306);
+            this.reportViewer1.TabIndex = 2;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // ReporteEmpleados_Jefe
+            // 
+            this.ReporteEmpleados_Jefe.DataSetName = "ReporteEmpleados_Jefe";
+            this.ReporteEmpleados_Jefe.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DataTable1BindingSource
+            // 
+            this.DataTable1BindingSource.DataMember = "DataTable1";
+            this.DataTable1BindingSource.DataSource = this.ReporteEmpleados_Jefe;
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_buscar.BackgroundImage = global::TuLuz.Properties.Resources.buscar;
+            this.btn_buscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_buscar.FlatAppearance.BorderSize = 0;
+            this.btn_buscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_buscar.Location = new System.Drawing.Point(278, 444);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(84, 64);
+            this.btn_buscar.TabIndex = 51;
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_cancelar.BackgroundImage = global::TuLuz.Properties.Resources.cancel_77947;
+            this.btn_cancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_cancelar.FlatAppearance.BorderSize = 0;
+            this.btn_cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cancelar.Location = new System.Drawing.Point(476, 436);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(90, 80);
+            this.btn_cancelar.TabIndex = 52;
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            this.btn_cancelar.Click += new System.EventHandler(this.btn_cancelar_Click);
+            // 
+            // Empleados_Jefes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 545);
+            this.Controls.Add(this.btn_cancelar);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.label1);
+            this.Name = "Empleados_Jefes";
+            this.Text = "Empleados_Jefes";
+            this.Load += new System.EventHandler(this.Empleados_Jefes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ReporteEmpleados_Jefe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataTable1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource DataTable1BindingSource;
+        private ReporteEmpleados_Jefe ReporteEmpleados_Jefe;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.Button btn_cancelar;
+    }
+}
---
+++ TuLuz/TuLuz/Forums/Reportes/Empleados_Jefes/Empleados_Jefes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TuLuz.Negocio;
+using TuLuz.Clases;
+using TuLuz.Negocio.EstructuraNegocios;
+using Microsoft.Reporting.WinForms;
+
+namespace TuLuz.Forums.Reportes.Empleados_Jefes
+{
+    public partial class Empleados_Jefes : Form
+    {
+        Ng_Empleados Empleados = new Ng_Empleados();
+        DataTable Tabla = new DataTable();
+
+        public Empleados_Jefes()
+        {
+            InitializeComponent();
+        }
+
+        private void Empleados_Jefes_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
