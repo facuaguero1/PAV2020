1ef3e1962bb1d7324e2ac85ef4ea6ccfec75e661
Agusto Portela <agustoportelaa@gmail.com>
2020-11-01 22:45:38 -0300

    Delete PROVEEDORES.pdf

--- TuLuz/TuLuz/Forums/Estadisticas/VendedoresXCotizaciones/Estadistica_VendedoresXCotizaciones.Designer.cs
+++ TuLuz/TuLuz/Forums/Estadisticas/VendedoresXCotizaciones/Estadistica_VendedoresXCotizaciones.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace TuLuz.Forums.Estadisticas.VentasXMes
+﻿namespace TuLuz.Forums.Estadisticas.VendedoresXCotizaciones
 {
-    partial class Estadistica_VentaXMes
+    partial class Estadistica_VendedoresXCotizaciones
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.rv_VentasXMes = new Microsoft.Reporting.WinForms.ReportViewer();
             this.label1 = new System.Windows.Forms.Label();
             this.btn_Buscar = new System.Windows.Forms.Button();
-            this.txt_CodProducto = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.RV_Proveedores_AC_NAC = new Microsoft.Reporting.WinForms.ReportViewer();
             this.SuspendLayout();
-            // 
-            // rv_VentasXMes
-            // 
-            this.rv_VentasXMes.LocalReport.ReportEmbeddedResource = "TuLuz.Forums.Estadisticas.VentasXMes.Rv_VentasXMes.rdlc";
-            this.rv_VentasXMes.Location = new System.Drawing.Point(91, 166);
-            this.rv_VentasXMes.Name = "rv_VentasXMes";
-            this.rv_VentasXMes.ServerReport.BearerToken = null;
-            this.rv_VentasXMes.Size = new System.Drawing.Size(836, 272);
-            this.rv_VentasXMes.TabIndex = 0;
             // 
             // label1
             // 
@@ -50,11 +39,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
             this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label1.Location = new System.Drawing.Point(98, 39);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(553, 46);
+            this.label1.Size = new System.Drawing.Size(916, 46);
             this.label1.TabIndex = 16;
-            this.label1.Text = "Ventas de Producto por mes";
+            this.label1.Text = "Vendedores con mayores cotizaciones ganadas";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // btn_Buscar
@@ -65,43 +54,34 @@
             this.btn_Buscar.FlatAppearance.BorderSize = 0;
             this.btn_Buscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Buscar.Location = new System.Drawing.Point(771, 98);
+            this.btn_Buscar.Location = new System.Drawing.Point(382, 87);
             this.btn_Buscar.Name = "btn_Buscar";
             this.btn_Buscar.Size = new System.Drawing.Size(62, 44);
             this.btn_Buscar.TabIndex = 20;
             this.btn_Buscar.UseVisualStyleBackColor = true;
-            this.btn_Buscar.Click += new System.EventHandler(this.btn_Buscar_Click);
             // 
-            // txt_CodProducto
+            // RV_Proveedores_AC_NAC
             // 
-            this.txt_CodProducto.Location = new System.Drawing.Point(348, 121);
-            this.txt_CodProducto.Name = "txt_CodProducto";
-            this.txt_CodProducto.Size = new System.Drawing.Size(100, 20);
-            this.txt_CodProducto.TabIndex = 21;
+            this.RV_Proveedores_AC_NAC.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RV_Proveedores_AC_NAC.LocalReport.ReportEmbeddedResource = "TuLuz.Forums.Estadisticas.VendedoresXCotizaciones.Rv_VendedoresXCotizaciones.rdlc" +
+    "";
+            this.RV_Proveedores_AC_NAC.Location = new System.Drawing.Point(46, 165);
+            this.RV_Proveedores_AC_NAC.Name = "RV_Proveedores_AC_NAC";
+            this.RV_Proveedores_AC_NAC.ServerReport.BearerToken = null;
+            this.RV_Proveedores_AC_NAC.Size = new System.Drawing.Size(849, 331);
+            this.RV_Proveedores_AC_NAC.TabIndex = 21;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(101, 116);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(220, 25);
-            this.label2.TabIndex = 22;
-            this.label2.Text = "Codigo de Producto";
-            // 
-            // Estadistica_VentaXMes
+            // Estadistica_VendedoresXCotizaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(959, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txt_CodProducto);
+            this.ClientSize = new System.Drawing.Size(936, 508);
+            this.Controls.Add(this.RV_Proveedores_AC_NAC);
             this.Controls.Add(this.btn_Buscar);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.rv_VentasXMes);
-            this.Name = "Estadistica_VentaXMes";
-            this.Text = "Estadistica_VentaXMes";
-            this.Load += new System.EventHandler(this.Estadistica_VentaXMes_Load);
+            this.Name = "Estadistica_VendedoresXCotizaciones";
+            this.Text = "Estadistica_VendedoresXCotizaciones";
+            this.Load += new System.EventHandler(this.Estadistica_VendedoresXCotizaciones_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,10 +89,8 @@
 
         #endregion
 
-        private Microsoft.Reporting.WinForms.ReportViewer rv_VentasXMes;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_Buscar;
-        private System.Windows.Forms.TextBox txt_CodProducto;
-        private System.Windows.Forms.Label label2;
+        private Microsoft.Reporting.WinForms.ReportViewer RV_Proveedores_AC_NAC;
     }
 }
---
+++ TuLuz/TuLuz/Forums/Estadisticas/VendedoresXCotizaciones/Estadistica_VendedoresXCotizaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuLuz.Forums.Estadisticas.VendedoresXCotizaciones
+{
+    public partial class Estadistica_VendedoresXCotizaciones : Form
+    {
+        public Estadistica_VendedoresXCotizaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void Estadistica_VendedoresXCotizaciones_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
